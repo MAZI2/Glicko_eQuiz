0bfe4740576cb7dae8de28b7f40e9d09c9d5f87d
Mac <matjazpogacnik5@gmail.com>
2023-12-26 11:41:04 +0100

    Computed glicko rating

--- prog/glicko.cpp
+++ prog/glicko.cpp
@@ -24,6 +24,7 @@ public:
     }
 
     double g(double rd) {
+        //printf("g %f", 1/sqrt(1+(3*q*q*rd*rd)/(M_PI*M_PI)));
         return 1/sqrt(1+(3*q*q*rd*rd)/(M_PI*M_PI));
     }
     double newR(Player &p, vector<pair<Player*, int>> &match) {
@@ -31,9 +32,17 @@ public:
         for(int i=0;i<match.size();i++) {
             a+=(p.g(match[i].first->rd)*(match[i].second-p.E(p, *match[i].first)));
         }
-        return r+(q/(1/(p.rd*p.rd)+1/p.dsq(p, match)))*a;
+        return p.r+(q/(1/(p.rd*p.rd)+1/p.dsq(p, match)))*a;
+    }
+    //ex
+    double newRSingle(Player &p, pair<Player*, int> &mpair) {
+        double a=0;
+
+        a+=(p.g(mpair.first->rd)*(mpair.second-p.E(p, *mpair.first)));
+        return p.r+(q/(1/(p.rd*p.rd)+1/p.dsqSingle(p, mpair)))*a;
     }
     double E(Player &p, Player &mp) {
+        //printf("E %f\n", 1/(1+pow(10, (-p.g(mp.rd)*(p.r-mp.r))/400)));
         return 1/(1+pow(10, (-p.g(mp.rd)*(p.r-mp.r))/400));
     }
     double dsq(Player &p, vector<pair<Player*, int>> &match) {
@@ -43,12 +52,43 @@ public:
         }
         return 1/(q*q*a);
     }
+    //ex m, p i
+    double dsqSingle(Player &p, pair<Player*, int> &mpair) {
+        double a=0;
+
+        a+=(pow(p.g(mpair.first->rd), 2)*p.E(p, *mpair.first)*(1-p.E(p, *mpair.first)));
+        //printf("dsq %f\n", 1/(q*q*a));
+        return 1/(q*q*a);
+    }
     double rdNew(Player &p, vector<pair<Player*, int>> &match) {
         return sqrt(1/(1/(p.rd*p.rd)+1/dsq(p, match)));
+    }
+    //ex
+    double rdNewSingle(Player &p, pair<Player*, int> &mpair) {
+        return sqrt(1/(1/(p.rd*p.rd)+1/dsqSingle(p, mpair)));
     }
 
 };
 
+int userRating(Player &p, vector<pair<Player*, int>> &match) {
+    p.r=p.newR(p, match);
+    printf("%f\n", p.r);
+    return p.r;
+}
+int exerciseRating(Player &p, vector<pair<Player*, int>> &match) {
+    for(auto [m, i] : match) {
+        pair<Player*, int> mpair={&p, !i};
+        printf("%f %f %d\n", m->r, mpair.first->r, mpair.second);
+
+
+        printf("%f  ", m->r);
+        m->r=p.newRSingle(*m, mpair);
+        m->rd=p.rdNewSingle(*m, mpair);
+        printf("%f %f\n", m->r, m->rd);
+    };
+
+    return 0;
+}
 
 int main() {
     Player p(1500.f, 350.f);
@@ -67,35 +107,11 @@ vector<pair<Player*, int>> ma = {{&a,1}};
 vector<pair<Player*, int>> mb = {{&b,1}};
 vector<pair<Player*, int>> mc = {{&c,1}};
 vector<pair<Player*, int>> md = {{&d,1}};
-    p.r=p.newR(p, ma);
-    p.rd=p.rdNew(p, ma);
-    cout<<p.r<<" "<<p.rd<<endl;
 
-    p.r=p.newR(p, mb);
-    p.rd=p.rdNew(p, mb);
-    cout<<p.r<<" "<<p.rd<<endl;
+cout << exerciseRating(p, match) << endl;
 
-    p.r=p.newR(p, mc);
-    p.rd=p.rdNew(p, mc);
-    cout<<p.r<<" "<<p.rd<<endl;
-
-    p.r=p.newR(p, md);
-    p.rd=p.rdNew(p, md);
-    cout<<p.r<<" "<<p.rd<<endl;
-
-    cout<<endl;
-    p.r=1500.f;
-    p.rd=350.f;
-
-    p.r=p.newR(p, ma);
-    p.rd=p.rdNew(p, ma);
-    cout<<p.r<<" "<<p.rd<<endl;
-
-    p.rdPeriod(p, 2);
-    p.r=p.newR(p, mc);
-    p.rd=p.rdNew(p, mc);
-
-    cout<<p.r<<" "<<p.rd<<endl;
-    
+//must calculate userRating with temporary array
+cout << userRating(p, match) << endl;
+   
     return 0;
 }
